4eb42fa98db7dfc93d4582f1d877c87d279be4d3
Misterun0m <135786378+Misterun0m@users.noreply.github.com>
2026-03-24 05:58:55 -0600

    Update CORS header for production domain

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Origin: https://jovenes-tramites.vercel.app");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
